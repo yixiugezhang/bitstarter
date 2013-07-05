661555eff93c0cf5938a7650fd69377c038ddfaf
Yixiuge Zhang <yixiugezhang@gmail.com>
2013-07-05 20:51:23 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(readMessage());
 });
-
+    
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
+var readMessage = function() {
+    fs.readFileSync('index.html', function (err, data) {
+        if (err) throw err;
+        return data.toString();
+    });
+}   
